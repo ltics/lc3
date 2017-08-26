141200980d26d98c63c0bbcb364f6692205a06af
zjhmale <zjhmale@gmail.com>
2017-08-27 01:16:25 +0800

    Add basic Parser

--- src/ast.hpp
+++ src/ast.hpp
@@ -4,74 +4,75 @@
 
 using namespace std;
 
-class Node {
-public:
-  virtual string token_literal() = 0;
-  virtual string to_string() = 0;
-};
+namespace ast {
+  class Node {
+  public:
+    virtual string token_literal() = 0;
+    virtual string to_string() = 0;
+  };
 
-class Statement : public Node {
-public:
-  string token_literal() {
-    return "statement literal";
-  }
-
-  string to_string() {
-    return "statement node";
-  }
-};
-
-class Expression : public Node {
-public:
-  string token_literal() {
-    return "expression literal";
-  }
-
-  string to_string() {
-    return "expression node";
-  }
-};
-
-typedef struct {
-  vector<Statement> statements;
-} Program;
-
-class Identifier : public Expression {
-private:
-  token::Token token;
-  string value;
-
-public:
-  string token_literal() {
-    return this->token.literal;
-  }
-
-  string to_string() {
-    return this->value;
-  }
-};
-
-class LetStatement : public Statement {
-private:
-  token::Token token;
-  shared_ptr<Identifier> name;
-  shared_ptr<Expression> value;
-
-public:
-  string token_literal() {
-    return this->token.literal;
-  }
-  string to_string() {
-    string s("");
-    s += this->token_literal();
-    s += " ";
-    s += this->name->to_string();
-    s += " = ";
-    if (this->value != nullptr) {
-      s += this->value->to_string();
+  class Statement : public Node {
+  public:
+    string token_literal() {
+      return "statement literal";
     }
-    s += ";";
-    return s;
-  }
-};
 
+    string to_string() {
+      return "statement node";
+    }
+  };
+
+  class Expression : public Node {
+  public:
+    string token_literal() {
+      return "expression literal";
+    }
+
+    string to_string() {
+      return "expression node";
+    }
+  };
+
+  typedef struct {
+    vector<Statement> statements;
+  } Program;
+
+  class Identifier : public Expression {
+  private:
+    token::Token token;
+    string value;
+
+  public:
+    string token_literal() {
+      return this->token.literal;
+    }
+
+    string to_string() {
+      return this->value;
+    }
+  };
+
+  class LetStatement : public Statement {
+  private:
+    token::Token token;
+    shared_ptr<Identifier> name;
+    shared_ptr<Expression> value;
+
+  public:
+    string token_literal() {
+      return this->token.literal;
+    }
+    string to_string() {
+      string s("");
+      s += this->token_literal();
+      s += " ";
+      s += this->name->to_string();
+      s += " = ";
+      if (this->value != nullptr) {
+        s += this->value->to_string();
+      }
+      s += ";";
+      return s;
+    }
+  };
+}
---
+++ src/parser.hpp
@@ -0,0 +1,39 @@
+#include "ast.hpp"
+#include "token.hpp"
+#include "lexer.hpp"
+#include <memory>
+
+namespace parser {
+  class Parser {
+  private:
+    shared_ptr<lexer::Lexer> lexer;
+    token::Token current_token;
+    token::Token peek_token;
+
+  public:
+    Parser(shared_ptr<lexer::Lexer> l);
+    static auto new_parser(shared_ptr<lexer::Lexer> l) -> shared_ptr<Parser>;
+    auto next_token() -> void;
+    auto parse_program() -> shared_ptr<ast::Program>;
+  };
+
+  Parser::Parser(shared_ptr<lexer::Lexer> l) {
+    this->lexer = l;
+  }
+
+  auto Parser::new_parser(shared_ptr<lexer::Lexer> l) -> shared_ptr<Parser> {
+    shared_ptr<Parser> p = shared_ptr<Parser>(new Parser(l));
+    p->next_token();
+    p->next_token();
+    return p;
+  }
+
+  auto Parser::next_token() -> void {
+    this->current_token = this->peek_token;
+    this->peek_token = lexer::next_token(this->lexer);
+  }
+
+  auto Parser::parse_program() -> shared_ptr<ast::Program> {
+    return nullptr;
+  }
+}

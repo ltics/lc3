d75e0502e2d68f084ca99e63a3f241dcc6f53430
zjhmale <zjhmale@gmail.com>
2017-08-29 13:22:24 +0800

    Finalize all statement ast nodes

--- src/ast.hpp
+++ src/ast.hpp
@@ -1,18 +1,29 @@
 #include "token.hpp"
 #include <memory>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 namespace ast {
   class Node {
+  protected:
+    token::Token token;
   public:
+    Node() {
+      token = {"empty", "empty"};
+    };
+    Node(token::Token t) {
+      token = t;
+    };
     virtual string token_literal() = 0;
     virtual string to_string() = 0;
   };
 
   class Statement : public Node {
   public:
+    Statement(): Node() {};
+    Statement(token::Token t): Node(t) {};
     string token_literal() {
       return "statement literal";
     }
@@ -24,6 +35,8 @@ namespace ast {
 
   class Expression : public Node {
   public:
+    Expression(): Node() {};
+    Expression(token::Token t): Node(t) {};
     string token_literal() {
       return "expression literal";
     }
@@ -33,16 +46,37 @@ namespace ast {
     }
   };
 
-  typedef struct {
-    vector<Statement> statements;
-  } Program;
+  class Program : public Node {
+  private:
+    vector<Statement> statements = {};
+
+  public:
+    Program(vector<Statement> stms): statements(stms) {};
+
+    string token_literal() {
+      if (this->statements.size() > 0) {
+        return this->statements[0].token_literal();
+      } else {
+        return "empty program";
+      }
+    }
+
+    string to_string() {
+      string s("");
+      for_each(statements.cbegin(), statements.cend(), [&](Statement stm) {
+          s += stm.to_string();
+        });
+      return s;
+    }
+  };
 
   class Identifier : public Expression {
   private:
-    token::Token token;
     string value;
 
   public:
+    Identifier(token::Token t, string v): Expression(t), value(v) {};
+
     string token_literal() {
       return this->token.literal;
     }
@@ -54,11 +88,12 @@ namespace ast {
 
   class LetStatement : public Statement {
   private:
-    token::Token token;
     shared_ptr<Identifier> name;
     shared_ptr<Expression> value;
 
   public:
+    LetStatement(token::Token t, shared_ptr<Identifier> n, shared_ptr<Expression> v): Statement(t), name(n), value(v) {};
+
     string token_literal() {
       return this->token.literal;
     }
@@ -72,6 +107,70 @@ namespace ast {
         s += this->value->to_string();
       }
       s += ";";
+      return s;
+    }
+  };
+
+  class ReturnStatement : public Statement {
+  private:
+    shared_ptr<Expression> value;
+
+  public:
+    ReturnStatement(token::Token t, shared_ptr<Expression> v): Statement(t), value(v) {};
+
+    string token_literal() {
+      return this->token.literal;
+    }
+
+    string to_string() {
+      string s("");
+      s += this->token_literal();
+      s += " ";
+      if (this->value != nullptr) {
+        s += this->value->to_string();
+      }
+      s += ";";
+      return s;
+    }
+  };
+
+  // Just a statement wrapper for toplevel expressions
+  class ExpressionStatement : public Statement {
+  private:
+    shared_ptr<Expression> expression;
+
+  public:
+    ExpressionStatement(token::Token t, shared_ptr<Expression> expr): Statement(t), expression(expr) {};
+
+    string token_literal() {
+      return this->token.literal;
+    }
+
+    string to_string() {
+      if (this->expression != nullptr) {
+        return this->expression->to_string();
+      } else {
+        return "";
+      }
+    }
+  };
+
+  class BlockStatement : public Statement {
+  private:
+    vector<Statement> statements;
+
+  public:
+    BlockStatement(token::Token t, vector<Statement> stms): Statement(t), statements(stms) {};
+
+    string token_literal() {
+      return this->token.literal;
+    }
+
+    string to_string() {
+      string s("");
+      for_each(statements.cbegin(), statements.cend(), [&](Statement stm) {
+          s += stm.to_string();
+        });
       return s;
     }
   };

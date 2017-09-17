6cc8881ba2e469d7cd55dd5b8aeb46cb00d2e375
zjhmale <zjhmale@gmail.com>
2017-09-17 16:12:34 +0800

    Finalize basic builtins

--- src/builtins.hpp
+++ src/builtins.hpp
@@ -4,9 +4,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <range/v3/all.hpp>
 
 using namespace std;
 using namespace fmt;
+using namespace ranges;
 using namespace object;
 
 namespace builtins {
@@ -24,11 +26,101 @@ namespace builtins {
       shared_ptr<String> str = static_pointer_cast<String>(o);
       return make_shared<Integer>(str->value.size());
     } else {
-      return make_shared<Error>(format("argument to 'len' not supported, got {0}", o->type()));
+      return make_shared<Error>(format("argument to `len` not supported, got {0}", o->type()));
     }
   }
 
+  auto puts_func(vector<shared_ptr<Object>> args) -> shared_ptr<Object> {
+    std::for_each(args.cbegin(), args.cend(), [](shared_ptr<Object> o) {
+        cout << o->inspect() << endl;
+      });
+
+    return make_shared<Null>();
+  }
+
+  auto first_func(vector<shared_ptr<Object>> args) -> shared_ptr<Object> {
+    if (args.size() != 1) {
+      string msg = format("wrong number of arguments. got={0}, want=1", args.size());
+      return make_shared<Error>(msg);
+    }
+
+    shared_ptr<Object> o = args[0];
+    if (o->type() != ARRAY_OBJ) {
+      return make_shared<Error>(format("argument to `first` must be ARRAY, got {0}", o->type()));
+    }
+
+    shared_ptr<Array> arr = static_pointer_cast<Array>(o);
+    if (arr->elements.size() > 0) {
+      return arr->elements[0];
+    } else {
+      return make_shared<Null>();
+    }
+  }
+
+  auto last_func(vector<shared_ptr<Object>> args) -> shared_ptr<Object> {
+    if (args.size() != 1) {
+      string msg = format("wrong number of arguments. got={0}, want=1", args.size());
+      return make_shared<Error>(msg);
+    }
+
+    shared_ptr<Object> o = args[0];
+    if (o->type() != ARRAY_OBJ) {
+      return make_shared<Error>(format("argument to `last` must be ARRAY, got {0}", o->type()));
+    }
+
+    shared_ptr<Array> arr = static_pointer_cast<Array>(o);
+    auto length = arr->elements.size();
+    if (length > 0) {
+      return arr->elements[length - 1];
+    } else {
+      return make_shared<Null>();
+    }
+  }
+
+  auto rest_func(vector<shared_ptr<Object>> args) -> shared_ptr<Object> {
+    if (args.size() != 1) {
+      string msg = format("wrong number of arguments. got={0}, want=1", args.size());
+      return make_shared<Error>(msg);
+    }
+
+    shared_ptr<Object> o = args[0];
+    if (o->type() != ARRAY_OBJ) {
+      return make_shared<Error>(format("argument to `rest` must be ARRAY, got {0}", o->type()));
+    }
+
+    shared_ptr<Array> arr = static_pointer_cast<Array>(o);
+    auto length = arr->elements.size();
+    if (length > 0) {
+      vector<shared_ptr<Object>> rest = arr->elements | view::tail;
+      return make_shared<Array>(rest);
+    } else {
+      return make_shared<Null>();
+    }
+  }
+
+  auto push_func(vector<shared_ptr<Object>> args) -> shared_ptr<Object> {
+    if (args.size() != 2) {
+      string msg = format("wrong number of arguments. got={0}, want=2", args.size());
+      return make_shared<Error>(msg);
+    }
+
+    shared_ptr<Object> o = args[0];
+    if (o->type() != ARRAY_OBJ) {
+      return make_shared<Error>(format("argument to `push` must be ARRAY, got {0}", o->type()));
+    }
+
+    shared_ptr<Array> arr = static_pointer_cast<Array>(o);
+    auto elements = arr->elements;
+    elements.push_back(args[1]);
+    return make_shared<Array>(elements);
+  }
+
   map<string, shared_ptr<Builtin>> builtins = {
-    { "let", make_shared<Builtin>(let_func) }
+    { "let", make_shared<Builtin>(let_func) },
+    { "puts", make_shared<Builtin>(puts_func) },
+    { "first", make_shared<Builtin>(first_func) },
+    { "last", make_shared<Builtin>(last_func) },
+    { "rest", make_shared<Builtin>(rest_func) },
+    { "push", make_shared<Builtin>(push_func) }
   };
 }
